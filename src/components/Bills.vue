c25305ac74636f18808b0181a63e8f3bfea605ab
kareen alkhateeb <150688138+kareen133@users.noreply.github.com>
2025-07-05 21:48:36 +0300

    Update Bills.vue

--- src/components/Bills.vue
+++ src/components/Bills.vue
@@ -11,42 +11,32 @@ const paidBillsTotal = transactions
 
   
 </script><template>
-  <div class="bg-white rounded-2xl p-6 w-full max-w-4xl mx-auto">
-    <div class="flex flex-col sm:flex-row justify-between mb-6 gap-4">
-      <p class="text-gray-500 text-sm">Recurring Bills</p>
-      <RouterLink
-        to="/recurring"
-        class="text-gray-500 text-sm cursor-pointer hover:underline self-start sm:self-center"
-      >
-        See Details
-      </RouterLink>
-    </div>
-
-    <div class="grid grid-cols-1 gap-4">
-      <div
-        class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
-      >
-        <p class="text-sm text-gray-600 mb-2">Paid Bill</p>
-        <p class="text-lg font-bold text-gray-800">
-          ${{ paidBillsTotal.toFixed(2) }}
-        </p>
-      </div>
-
-      <div
-        class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
-      >
-        <p class="text-sm text-gray-600 mb-2">Total Upcoming</p>
-        <p class="text-lg font-bold text-gray-800">$194.98</p>
-      </div>
-
-      <div
-        class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
-      >
-        <p class="text-sm text-gray-600 mb-2">Due Soon</p>
-        <p class="text-lg font-bold text-gray-800">$59.98</p>
-      </div>
-    </div>
+ 
+   <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+  <div
+    class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
+  >
+    <p class="text-sm text-gray-600 mb-2">Paid Bill</p>
+    <p class="text-lg font-bold text-gray-800">
+      ${{ paidBillsTotal.toFixed(2) }}
+    </p>
   </div>
+
+  <div
+    class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
+  >
+    <p class="text-sm text-gray-600 mb-2">Total Upcoming</p>
+    <p class="text-lg font-bold text-gray-800">$194.98</p>
+  </div>
+
+  <div
+    class="flex flex-col justify-between bg-white p-4 rounded-lg text-center shadow-lg"
+  >
+    <p class="text-sm text-gray-600 mb-2">Due Soon</p>
+    <p class="text-lg font-bold text-gray-800">$59.98</p>
+  </div>
+</div>
+
 </template>
 
 <style scoped>
